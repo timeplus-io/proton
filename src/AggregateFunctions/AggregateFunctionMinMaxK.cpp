cee6b96d52c8b12c5e1b9658db3b627c240bb41f
Lisen <38773813+yl-lisen@users.noreply.github.com>
2022-02-18 14:47:53 +0800

    Feature/support min_k/max_k multi-column compare and output extra context column (#409)
    This close #257

--- src/AggregateFunctions/AggregateFunctionMinMaxK.cpp
+++ src/AggregateFunctions/AggregateFunctionMinMaxK.cpp
@@ -4,10 +4,10 @@
 #include "FactoryHelpers.h"
 #include "Helpers.h"
 
-#include <Common/FieldVisitorConvertToNumber.h>
 #include <DataTypes/DataTypeDate.h>
 #include <DataTypes/DataTypeDateTime.h>
-
+#include <Functions/FunctionHelpers.h>
+#include <Common/FieldVisitorConvertToNumber.h>
 
 namespace DB
 {
@@ -61,45 +61,249 @@ namespace
     AggregateFunctionPtr
     createAggregateFunctionMinMaxK(const std::string & name, const DataTypes & argument_types, const Array & params, const Settings *)
     {
-        assertUnary(name, argument_types);
+        if (argument_types.size() < 1)
+            throw Exception(ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH, "Aggregate function {} requires at least one argument.", name);
+
         UInt64 k = 10; /// default values
 
         if (!params.empty())
         {
             if (params.size() > 1)
-                throw Exception("Aggregate function " + name + " requires one parameters.", ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+                throw Exception(ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH, "Aggregate function {} requires one parameter.", name);
 
             k = applyVisitor(FieldVisitorConvertToNumber<UInt64>(), params[0]);
             if (k > TOP_K_MAX_SIZE)
+
                 throw Exception(
-                    "Too large parameter(s) for aggregate function " + name + ". Maximum: " + toString(TOP_K_MAX_SIZE),
-                    ErrorCodes::ARGUMENT_OUT_OF_BOUND);
+                    ErrorCodes::ARGUMENT_OUT_OF_BOUND,
+                    "Too large parameter(s) for aggregate function {}. Maximum: {}",
+                    name,
+                    TOP_K_MAX_SIZE);
 
             if (k == 0)
-                throw Exception("Parameter 0 is illegal for aggregate function " + name, ErrorCodes::ARGUMENT_OUT_OF_BOUND);
+                throw Exception(ErrorCodes::ARGUMENT_OUT_OF_BOUND, "Parameter 0 is illegal for aggregate function {}", name);
         }
 
-        AggregateFunctionPtr res(createWithNumericType<AggregateFunctionMinMaxK, is_min>(*argument_types[0], k, argument_types[0], params));
-
-        if (!res)
-            res = AggregateFunctionPtr(createWithExtraTypes<is_min>(argument_types[0], k, params));
+        AggregateFunctionPtr res;
+        if (argument_types.size() == 1)
+        {
+            res.reset(createWithNumericType<AggregateFunctionMinMaxK, is_min>(*argument_types[0], k, argument_types[0], params));
+            if (!res)
+                res = AggregateFunctionPtr(createWithExtraTypes<is_min>(argument_types[0], k, params));
+        }
+        else
+        {
+            res = AggregateFunctionPtr(new AggregateFunctionMinMaxKTuple<is_min>(k, argument_types, params));
+        }
 
         if (!res)
             throw Exception(
-                "Illegal type " + argument_types[0]->getName() + " of argument for aggregate function " + name,
-                ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+                ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
+                "Illegal type {} of argument for aggregate function {}",
+                argument_types[0]->getName(),
+                name);
 
         return res;
     }
 
+
+    template <typename TYPE, bool is_min>
+    constexpr decltype(auto) buildValueComparer()
+    {
+        return [](const std::any & l, const std::any & r) {
+            assert(l.type() == typeid(TYPE));
+            assert(r.type() == typeid(TYPE));
+            if constexpr (is_min)
+                return DB::CompareHelper<TYPE>::compare(
+                    std::any_cast<const TYPE &>(l), std::any_cast<const TYPE &>(r), /* nan_direction_hint */ 1);
+            else
+                return DB::CompareHelper<TYPE>::compare(
+                    std::any_cast<const TYPE &>(l), std::any_cast<const TYPE &>(r), /* nan_direction_hint */ -1);
+        };
+    }
+
+    template <typename TYPE, TypeCategory type_category>
+    constexpr decltype(auto) buildValueGetter(size_t col_index)
+    {
+        return [=](const DB::IColumn ** columns, size_t row_num, Arena * arena) -> std::pair<std::any, TypeCategory> {
+            if constexpr (type_category == TypeCategory::SEIRIALIZED_STRING_REF)
+            {
+                const char * begin = nullptr;
+                StringRef str_serialized = columns[col_index]->serializeValueIntoArena(row_num, *arena, begin);
+                return {str_serialized, type_category};
+            }
+            else if constexpr (type_category == TypeCategory::STRING_REF)
+                return {columns[col_index]->getDataAt(row_num), type_category};
+            else
+            {
+                const auto & column = assert_cast<const DB::ColumnVector<TYPE> &>(*columns[col_index]);
+                return {column.getElement(row_num), type_category};
+            }
+        };
+    }
+
+    template <typename TYPE, TypeCategory type_category>
+    constexpr decltype(auto) buildValueAppender(size_t tuple_element_index)
+    {
+        return [=](const std::any & val, DB::ColumnTuple & to_column) {
+            if constexpr (type_category == TypeCategory::SEIRIALIZED_STRING_REF)
+            {
+                const auto & string_ref = std::any_cast<const TYPE &>(val);
+                to_column.getColumn(tuple_element_index).deserializeAndInsertFromArena(string_ref.data);
+            }
+            else if constexpr (type_category == TypeCategory::STRING_REF)
+            {
+                const auto & string_ref = std::any_cast<const TYPE &>(val);
+                to_column.getColumn(tuple_element_index).insertData(string_ref.data, string_ref.size);
+            }
+            else
+            {
+                auto & column = assert_cast<DB::ColumnVector<TYPE> &>(to_column.getColumn(tuple_element_index));
+                column.insertValue(std::any_cast<const TYPE &>(val));
+            }
+        };
+    }
+
+    template <typename TYPE>
+    constexpr decltype(auto) buildValueWriter()
+    {
+        return [](const std::any & val, WriteBuffer & buf) { writeBinary(std::any_cast<const TYPE &>(val), buf); };
+    }
+
+    template <typename TYPE>
+    constexpr decltype(auto) buildValueReader()
+    {
+        return [](ReadBuffer & buf, Arena * arena) -> std::pair<std::any, size_t> {
+            if constexpr (std::is_same_v<TYPE, StringRef>)
+            {
+                auto string_ref = readStringBinaryInto(*arena, buf);
+                return {string_ref, string_ref.size};
+            }
+            else
+            {
+                TYPE val;
+                readBinary(val, buf);
+                return {val, 0};
+            }
+        };
+    }
 }
+
+#define DISPATCH(TYPE, M, ...) \
+    do \
+    { \
+        switch (WhichDataType(TYPE).idx) \
+        { \
+            case TypeIndex::Date: \
+                M(DataTypeDate::FieldType, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::DateTime: \
+                M(typename DataTypeDateTime::FieldType, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::Enum8: [[fallthrough]]; \
+            case TypeIndex::UInt8: \
+                M(UInt8, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::Enum16: [[fallthrough]]; \
+            case TypeIndex::UInt16: \
+                M(UInt16, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::UInt32: \
+                M(UInt32, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::UInt64: \
+                M(UInt64, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::UInt128: \
+                M(UInt128, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::UInt256: \
+                M(UInt256, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::Int8: \
+                M(Int8, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::Int16: \
+                M(Int16, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::Int32: \
+                M(Int32, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::Int64: \
+                M(Int64, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::Int128: \
+                M(Int128, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::Int256: \
+                M(Int256, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::Float32: \
+                M(Float32, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            case TypeIndex::Float64: \
+                M(Float64, TypeCategory::NUMERIC, ##__VA_ARGS__); break; \
+            default: \
+            { \
+                if (TYPE->isValueUnambiguouslyRepresentedInContiguousMemoryRegion()) \
+                    M(StringRef, TypeCategory::STRING_REF, ##__VA_ARGS__); \
+                else \
+                    M(StringRef, TypeCategory::SEIRIALIZED_STRING_REF, ##__VA_ARGS__); \
+            } \
+        } \
+    } while (0)
+
+template <bool is_min>
+void AggregateFunctionMinMaxKTuple<is_min>::buildTupleValueOperators(TupleOperators & operators) const
+{
+    assert(this->argument_types.size() > 1);
+
+#define BUILD_PARTIAL_COMPARER(TYPE, TYPE_CATEGORY, ...) operators.comparers.emplace_back(buildValueComparer<TYPE, is_min>(__VA_ARGS__))
+#define BUILD_VALUE_GETTER(TYPE, TYPE_CATEGORY, ...) operators.getters.emplace_back(buildValueGetter<TYPE, TYPE_CATEGORY>(__VA_ARGS__))
+#define BUILD_VALUE_APPENDER(TYPE, TYPE_CATEGORY, ...) \
+    operators.appenders.emplace_back(buildValueAppender<TYPE, TYPE_CATEGORY>(__VA_ARGS__))
+#define BUILD_VALUE_WRITER(TYPE, TYPE_CATEGORY, ...) operators.writers.emplace_back(buildValueWriter<TYPE>(__VA_ARGS__))
+#define BUILD_VALUE_READER(TYPE, TYPE_CATEGORY, ...) operators.readers.emplace_back(buildValueReader<TYPE>(__VA_ARGS__))
+
+    for (size_t col_idx = 0; col_idx < this->argument_types.size(); ++col_idx)
+    {
+        const auto & arg_type = this->argument_types[col_idx];
+        /// Compare by first argument
+        if (col_idx == 0)
+            DISPATCH(arg_type, BUILD_PARTIAL_COMPARER);
+
+        /// Get value from columns[col_idx] (Column)
+        DISPATCH(arg_type, BUILD_VALUE_GETTER, col_idx);
+        DISPATCH(arg_type, BUILD_VALUE_APPENDER, col_idx);
+        DISPATCH(arg_type, BUILD_VALUE_WRITER);
+        DISPATCH(arg_type, BUILD_VALUE_READER);
+    }
+
+    assert(operators.comparers.size() == 1);
+    assert(operators.getters.size() == this->argument_types.size());
+    assert(operators.appenders.size() == this->argument_types.size());
+    assert(operators.writers.size() == this->argument_types.size());
+    assert(operators.readers.size() == this->argument_types.size());
+#undef BUILD_PARTIAL_COMPARE
+#undef BUILD_VALUE_GETTER
+#undef BUILD_VALUE_APPENDER
+#undef BUILD_VALUE_WRITER
+#undef BUILD_VALUE_READER
+}
+
+template <bool is_min>
+void AggregateFunctionMinMaxKTuple<is_min>::insertResultInto(AggregateDataPtr __restrict place, IColumn & to, Arena *) const
+{
+    auto & arr_to = assert_cast<ColumnArray &>(to);
+    auto & tuple_to = assert_cast<ColumnTuple &>(arr_to.getData());
+    ColumnArray::Offsets & offsets_to = arr_to.getOffsets();
+
+    auto & top_k = this->data(place);
+    top_k.sort();
+
+    for (const auto & tuple_value : top_k)
+    {
+        assert(tuple_value.values.size() == top_k.operators.appenders.size());
+        for (size_t i = 0; i < top_k.operators.appenders.size(); ++i)
+            top_k.operators.appenders[i](tuple_value.values[i], tuple_to);
+    }
+
+    offsets_to.push_back(tuple_to.size());
+}
+
+#undef DISPATCH
 
 void registerAggregateFunctionMinMaxK(AggregateFunctionFactory & factory)
 {
     AggregateFunctionProperties properties = {.returns_default_when_only_null = false, .is_order_dependent = true};
 
-    factory.registerFunction("maxK", {createAggregateFunctionMinMaxK<false>, properties});
-    factory.registerFunction("minK", {createAggregateFunctionMinMaxK<true>, properties});
+    factory.registerFunction("max_k", {createAggregateFunctionMinMaxK<false>, properties});
+    factory.registerFunction("min_k", {createAggregateFunctionMinMaxK<true>, properties});
 }
 
 }
--- src/AggregateFunctions/AggregateFunctionMinMaxK.h
+++ src/AggregateFunctions/AggregateFunctionMinMaxK.h
@@ -3,13 +3,40 @@
 #include "IAggregateFunction.h"
 
 #include <Columns/ColumnArray.h>
-#include <Common/SpaceSaving.h>
-#include <Common/assert_cast.h>
 #include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypeString.h>
+#include <DataTypes/DataTypeTuple.h>
 #include <DataTypes/DataTypesNumber.h>
 #include <IO/ReadHelpers.h>
 #include <IO/WriteHelpers.h>
+#include <Common/SpaceSaving.h>
+#include <Common/assert_cast.h>
+
+#include <any>
+
+/// min_k/max_k(k)(compare_column(s) [,context_1, context_2 ...])
+/// NOTE:
+/// arguments:
+///   - Compare: first argument is the compare column(s), which supports: single-column or multi-columns(tuple)
+///   - Context: other arguments supports: omitted, single-column, multi-columns, many columns
+/// Examples:
+/// [table]
+///    id, value, name
+///     1, 20, 'hhh'
+///     1, 10, 'mmm'
+///     2, 15, 'tp'
+///     3. 18, 'bbb'
+///
+/// [query]
+/// Only output compare columns:
+/// 1) min_k(3)(id)                             ->  result: [1, 1, 2]
+/// 2) min_k(3)(tuple(id, value))               ->  result: [(1,10), (1,20), (2,15)]
+///
+/// output compare + context columns:
+/// 3) min_k(3)(id, value)                      ->  result: [(1,20), (1,10), (2,15)]
+/// 4) min_k(3)(id, value, name)                ->  result: [(1,20,'hhh'), (1,10,'mmm'), (2,15,'tp')]
+/// 5) min_k(3)(tuple(id, value), name)         ->  result: [(1,10,'mmm'), (1,20,'hhh'), (2,15,'tp')]
+/// 6) min_k(3)(id, *)                          ->  result: [(1,1,20,'hhh'), (1,1,10,'mmm'), (2,2,15,'tp')]
 
 namespace DB
 {
@@ -130,7 +157,7 @@ public:
     {
     }
 
-    String getName() const override { return is_min ? "minK" : "maxK"; }
+    String getName() const override { return is_min ? "min_k" : "max_k"; }
 
     DataTypePtr getReturnType() const override { return std::make_shared<DataTypeArray>(this->argument_types[0]); }
 
@@ -152,7 +179,10 @@ public:
             top_k.add(elem, k);
     }
 
-    void serialize(ConstAggregateDataPtr __restrict place, WriteBuffer & buf, std::optional<size_t> /*version*/) const override { this->data(place).write(buf); }
+    void serialize(ConstAggregateDataPtr __restrict place, WriteBuffer & buf, std::optional<size_t> /*version*/) const override
+    {
+        this->data(place).write(buf);
+    }
 
     void deserialize(AggregateDataPtr __restrict place, ReadBuffer & buf, std::optional<size_t> /*version*/, Arena *) const override
     {
@@ -211,13 +241,16 @@ public:
     {
     }
 
-    String getName() const override { return is_min ? "minK" : "maxK"; }
+    String getName() const override { return is_min ? "min_k" : "max_k"; }
 
     DataTypePtr getReturnType() const override { return std::make_shared<DataTypeArray>(input_data_type); }
 
     bool allocatesMemoryInArena() const override { return true; }
 
-    void serialize(ConstAggregateDataPtr __restrict place, WriteBuffer & buf, std::optional<size_t>) const override { this->data(place).write(buf); }
+    void serialize(ConstAggregateDataPtr __restrict place, WriteBuffer & buf, std::optional<size_t>) const override
+    {
+        this->data(place).write(buf);
+    }
 
     void deserialize(AggregateDataPtr __restrict place, ReadBuffer & buf, std::optional<size_t> /*version*/, Arena * arena) const override
     {
@@ -285,4 +318,239 @@ public:
     }
 };
 
+enum class TypeCategory : UInt8
+{
+    NUMERIC,
+    STRING_REF,
+    SEIRIALIZED_STRING_REF,
+    OTHERS,
+};
+
+struct TupleOperators
+{
+    using Comparer = std::function<int(const std::any &, const std::any &)>;
+    using Getter = std::function<std::pair<std::any, TypeCategory>(const IColumn **, size_t, Arena *)>;
+    using Appender = std::function<void(const std::any &, ColumnTuple &)>;
+    using Writer = std::function<void(const std::any &, WriteBuffer &)>;
+    using Reader = std::function<std::pair<std::any, size_t /*alloc_size*/>(ReadBuffer &, Arena *)>;
+
+    std::vector<Comparer> comparers;    /// Tuple element comparers
+    std::vector<Getter> getters;        /// Tuple element retrievers
+    std::vector<Appender> appenders;    /// Tuple element appender. Append one element to ColumnTuple
+    std::vector<Writer> writers;        /// Tuple element writers
+    std::vector<Reader> readers;        /// Tuple element readers
+};
+
+struct TupleValue
+{
+    std::vector<std::any> values;
+    std::vector<size_t> string_ref_indexs;
+    const TupleOperators * operators;
+
+    TupleValue(std::vector<std::any> values_, std::vector<size_t> string_ref_indexs_, const TupleOperators & operators_)
+        : values(std::move(values_)), string_ref_indexs(std::move(string_ref_indexs_)), operators(&operators_)
+    {
+    }
+
+    bool operator<(const TupleValue & val) const
+    {
+        assert(operators->comparers.size() <= values.size());
+        assert(operators->comparers.size() <= val.values.size());
+        for (size_t i = 0; i < operators->comparers.size(); ++i)
+        {
+            auto res = operators->comparers[i](values[i], val.values[i]);
+            if (res < 0)
+                return true;
+            else if (res > 0)
+                return false;
+        }
+        return false;
+    }
+
+    bool operator>(const TupleValue & val) const
+    {
+        assert(operators->comparers.size() <= values.size());
+        assert(operators->comparers.size() <= val.values.size());
+        for (size_t i = 0; i < operators->comparers.size(); ++i)
+        {
+            auto res = operators->comparers[i](values[i], val.values[i]);
+            if (res > 0)
+                return true;
+            else if (res < 0)
+                return false;
+        }
+        return false;
+    }
+
+    void write(WriteBuffer & buf) const
+    {
+        assert(operators->writers.size() == values.size());
+        for (size_t i = 0; i < operators->writers.size(); ++i)
+            operators->writers[i](values[i], buf);
+
+        writeVectorBinary(string_ref_indexs, buf);
+    }
+};
+
+template <>
+struct SpaceSavingArena<TupleValue>
+{
+    const TupleValue emplace(const TupleValue & key)
+    {
+        TupleValue new_tuple_value(key);
+        for (auto string_ref_idx : key.string_ref_indexs)
+        {
+            const auto & string_ref = std::any_cast<const StringRef &>(key.values[string_ref_idx]);
+            auto ptr = arena.alloc(string_ref.size);
+            std::copy(string_ref.data, string_ref.data + string_ref.size, ptr);
+            new_tuple_value.values[string_ref_idx] = StringRef{ptr, string_ref.size};
+        }
+        return new_tuple_value;
+    }
+
+    void free(const TupleValue & key)
+    {
+        for (auto string_ref_idx : key.string_ref_indexs)
+        {
+            const auto & string_ref = std::any_cast<const StringRef &>(key.values[string_ref_idx]);
+            arena.free(const_cast<char *>(string_ref.data), string_ref.size);
+        }
+    }
+
+private:
+    ArenaWithFreeLists arena;
+};
+
+inline void writeBinary(const TupleValue & x, WriteBuffer & buf)
+{
+    x.write(buf);
 }
+
+template <bool is_min>
+struct AggregateFunctionMinMaxKTupleData : AggregateFunctionMinMaxKData<TupleValue, is_min>
+{
+    TupleOperators operators;
+
+    void read(ReadBuffer & rb, Arena * arena_)
+    {
+        size_t size = 0;
+        readVarUInt(size, rb);
+
+        assert(size < TOP_K_MAX_SIZE);
+
+        /// We free current values before read.
+        for (const auto & value : this->values)
+            this->arena.free(value);
+
+        this->values.reserve(size);
+        for (size_t i = 0; i < size; ++i)
+        {
+            std::vector<std::any> tuple_values;
+            tuple_values.reserve(operators.readers.size());
+            size_t arena_allocated_size = 0;
+            for (size_t idx = 0; idx < operators.readers.size(); ++idx)
+            {
+                auto [val, alloc_size] = operators.readers[idx](rb, arena_);
+                tuple_values.push_back(val);
+                arena_allocated_size += alloc_size;
+            }
+
+            std::vector<size_t> string_ref_indexs;
+            readVectorBinary(string_ref_indexs, rb);
+            this->values.push_back(TupleValue(std::move(tuple_values), std::move(string_ref_indexs), operators));
+            arena_->rollback(arena_allocated_size);
+        }
+
+        readBoolText(this->is_sorted, rb);
+    }
+};
+
+template <bool is_min>
+class AggregateFunctionMinMaxKTuple
+    : public IAggregateFunctionDataHelper<AggregateFunctionMinMaxKTupleData<is_min>, AggregateFunctionMinMaxKTuple<is_min>>
+{
+protected:
+    using State = AggregateFunctionMinMaxKTupleData<is_min>;
+    UInt64 k;
+    UInt64 reserved;
+
+public:
+    AggregateFunctionMinMaxKTuple(UInt64 k_, const DataTypes & arguments, const Array & params)
+        : IAggregateFunctionDataHelper<AggregateFunctionMinMaxKTupleData<is_min>, AggregateFunctionMinMaxKTuple<is_min>>(arguments, params)
+        , k(k_)
+        , reserved(k_ + 1)
+    {
+    }
+
+protected:
+    void buildTupleValueOperators(TupleOperators & operators) const;
+
+public:
+    String getName() const override { return is_min ? "min_k" : "max_k"; }
+
+    DataTypePtr getReturnType() const override
+    {
+        assert(this->argument_types.size() > 1);
+        return std::make_shared<DataTypeArray>(std::make_shared<DataTypeTuple>(this->argument_types));
+    }
+
+    bool allocatesMemoryInArena() const override { return false; }
+
+    void add(AggregateDataPtr __restrict place, const IColumn ** columns, size_t row_num, Arena * arena) const override
+    {
+        auto & top_k = this->data(place);
+        std::vector<std::any> tuple_value;
+        std::vector<size_t> string_ref_indexs;
+        size_t arena_allocated_size = 0;
+        for (const auto & getter : top_k.operators.getters)
+        {
+            auto [val, type_category] = getter(columns, row_num, arena);
+            if (type_category == TypeCategory::STRING_REF)
+            {
+                string_ref_indexs.push_back(tuple_value.size());
+            }
+            else if (type_category == TypeCategory::SEIRIALIZED_STRING_REF)
+            {
+                string_ref_indexs.push_back(tuple_value.size());
+                arena_allocated_size += std::any_cast<const StringRef &>(val).size;
+            }
+
+            tuple_value.push_back(std::move(val));
+        }
+
+        top_k.add(TupleValue(std::move(tuple_value), std::move(string_ref_indexs), top_k.operators), k);
+        arena->rollback(arena_allocated_size);
+    }
+
+    void merge(AggregateDataPtr __restrict place, ConstAggregateDataPtr rhs, Arena *) const override
+    {
+        auto & top_k = this->data(place);
+
+        for (const auto & elem : this->data(rhs))
+            top_k.add(elem, k);
+    }
+
+    void serialize(ConstAggregateDataPtr __restrict place, WriteBuffer & buf, std::optional<size_t> /*version*/) const override
+    {
+        this->data(place).write(buf);
+    }
+
+    void deserialize(AggregateDataPtr __restrict place, ReadBuffer & buf, std::optional<size_t> /*version*/, Arena * arena) const override
+    {
+        auto & top_k = this->data(place);
+        top_k.read(buf, arena);
+    }
+
+    void insertResultInto(AggregateDataPtr __restrict place, IColumn & to, Arena *) const override;
+
+    void create(AggregateDataPtr __restrict place) const override
+    {
+        new (place) State;
+        auto & data = this->data(place);
+        data.reserve(reserved);
+        this->buildTupleValueOperators(data.operators);
+    }
+};
+}
+
+#undef BUILD_COLUMN_COMPARE
