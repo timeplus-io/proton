6d4c7c5652e8b695dbef2f7d5eebdd71016fcdd2
Qijun Niu <qijun.niu@timeplus.io>
2022-03-23 09:21:15 +0800

    append leak sanitize when sanitizer=address (#615)

--- cmake/sanitize.cmake
+++ cmake/sanitize.cmake
@@ -16,7 +16,7 @@ endmacro()
 
 if (SANITIZE)
     if (SANITIZE STREQUAL "address")
-        set (ASAN_FLAGS "-fsanitize=address -fsanitize-address-use-after-scope")
+        set (ASAN_FLAGS "-fsanitize=address -fsanitize-address-use-after-scope -fsanitize=leak")
         set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${SAN_FLAGS} ${ASAN_FLAGS}")
         set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${SAN_FLAGS} ${ASAN_FLAGS}")
 
