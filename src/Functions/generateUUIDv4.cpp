956a1f0651ba6ed6deeb3daf1fad2ea03cacc798
Qijun Niu <qijun.niu@timeplus.io>
2022-08-09 01:03:33 +0800

    rename function generate_uuidv4 to uuid (#1145)
    This closes #1096

--- src/Functions/generateUUIDv4.cpp
+++ src/Functions/generateUUIDv4.cpp
@@ -14,7 +14,7 @@ DECLARE_SEVERAL_IMPLEMENTATIONS(
 class FunctionGenerateUUIDv4 : public IFunction
 {
 public:
-    static constexpr auto name = "generate_uuidv4";
+    static constexpr auto name = "uuid";
 
     String getName() const override
     {
