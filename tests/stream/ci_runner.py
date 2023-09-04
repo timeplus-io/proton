40f8767068bf9a2a3658a1557c2fc243ee8fe62c
jhao0117 <james@timeplus.io>
2023-09-04 15:37:00 +0800

    Update ci_runner.py

--- tests/stream/ci_runner.py
+++ tests/stream/ci_runner.py
@@ -28,12 +28,10 @@ docker_compose_file_path = f"{cur_dir}/test_stream_smoke/configs/docker-compose.
 DEFAULT_TEST_SUITE_TIMEOUT = 900 #seconds
 DEFAULT_TEST_FOLDER = "test_stream_smoke"
 
-
 def compress_logs(self, dir, relpaths, result_path):
     subprocess.check_call(
         "tar czf {} -C {} {}".format(result_path, dir, " ".join(relpaths)), shell=True
     )  # STYLE_CHECK_ALLOW_SUBPROCESS_CHECK_CALL
-
 
 def container_file_download(dir="./", setting="nativelog", *files_in_container):
     print(files_in_container)
@@ -56,7 +54,6 @@ def container_file_download(dir="./", setting="nativelog", *files_in_container):
     else:
         return ()
 
-
 def upload_results(
     s3_client,
     report_file_path,
@@ -73,7 +70,6 @@ def upload_results(
         report_file_path, s3_path_prefix + s3_report_name
     )
     report_urls.append(report_url)
-    
     if len(additional_files_paths) > 0:
         for additional_file_path in additional_files_paths:
             logging.debug(
@@ -84,10 +80,8 @@ def upload_results(
                 additional_file_path, s3_path_prefix + file_name
             )
             report_urls.append(url)
-
     logging.info("Search result in url %s", report_urls)
     return report_urls
-
 
 def upload_proton_logs(s3_client, proton_log_folder, pr_number=0, commit_sha=""):
     s3_proton_log_folder = (
@@ -98,7 +92,6 @@ def upload_proton_logs(s3_client, proton_log_folder, pr_number=0, commit_sha="")
     )
     logging.info("Search result in url %s", proton_log_url)
     return proton_log_url
-
 
 def proton_python_driver_install():
     s3_helper = S3Helper("https://s3.amazonaws.com")
@@ -133,12 +126,7 @@ def proton_python_driver_install():
         logger.debug(
             f"{PROTON_PYTHON_DRIVER_NANME} exists bypass s3 download and install"
         )
-
     time.sleep(1)
-
-
-
-
 
 def ci_runner(
     local_all_results_folder_path,
@@ -151,7 +139,6 @@ def ci_runner(
     setting="default",
     logging_level="INFO",
 ):
-    
     timestamp = str(datetime.datetime.now())
     setting_running_start = datetime.datetime.now()
     report_file_name = f"report_{setting}_{timestamp}.html"
@@ -168,9 +155,7 @@ def ci_runner(
     if multi_protons == True:#if multi_protons is True, there are multiple settings for allocating the test suites on configs
         for key in setting_config["settings"]:
             proton_server_container_name_str = setting_config["settings"][key].get("proton_server_container_name")
-
             ci_runner_params_from_config = setting_config["settings"][key].get("ci_runner_params") #todo: support ci_runner_params per env of multi envs setting
-
             if proton_server_container_name_str is None:
                 raise Exception(f"proton_server_container_name of setting = {setting} is not found in setting_config")
             else:
@@ -180,9 +165,7 @@ def ci_runner(
         if proton_server_container_name_str is None:
             raise Exception(f"proton_server_container_name of setting = {setting} is not found in setting_config")
         proton_server_container_name_list = proton_server_container_name_str.split(',') #for multi containers in clustering settings
-        #proton_server_container_name = proton_server_container_name_list[0] #todo: handle multi containers in clustering scenario
         ci_runner_params_from_config = setting_config.get("ci_runner_params")
-        #print(f"ci_runner: ci_runner_params_from_config = {ci_runner_params_from_config}") #todo: currently only single env setting support ci_runner_params, need to optimize to support ci_runner_params per env
         if ci_runner_params_from_config is not None and len(ci_runner_params_from_config) > 0: #todo: currently only single env setting support ci_runner_params, need to optimize to support ci_runner_params per env
             for param in ci_runner_params_from_config:
                 print(f"ci_runner: setting = {setting}, param = {param}")
@@ -191,11 +174,6 @@ def ci_runner(
                     env_setting = os.getenv(key)
                     print(f"os.getenv({key}) = {env_setting}")        
     os.environ["PROTON_SETTING"] = setting # set the env virable to setting for rockets_run() based on settings gotten from cmdline
-    #set env vars for rockets_run based on the other ci_runner parameters gotten from config
-
-                
-    
-    #set proton log container path therefore log files could be retrieved later.
     proton_logs_in_container = [] # a list of tuple
     print(f"proton_server_container_name_list = {proton_server_container_name_list}")
     for proton_server_container_name in proton_server_container_name_list:
@@ -207,7 +185,6 @@ def ci_runner(
             f"{proton_server_container_name}://var/log/proton-server/proton-server.err.log"
         )
         proton_logs_in_container.append((proton_server_container_name, proton_err_log_in_container))
-    
     pytest_args =         [
             "-s",
             "-v",
@@ -217,45 +194,33 @@ def ci_runner(
             f"--html={report_file_path}",
             "--self-contained-html",
         ]
-    
     for test_folder in test_folders_list:
         pytest_args.append(test_folder)
-   
     retcode = pytest.main(
         pytest_args
     )
-    
     if test_result_shared_list != None:
         test_result_shared_list.append(
             {"proton_setting": setting, "retcode": retcode}
         )
-
     setting_running_test_end = datetime.datetime.now()
     setting_test_duration = setting_running_test_end - setting_running_start
-
     print(f"setting = {setting}, setting_test_duration = {setting_test_duration.seconds}")
 
     with open(".status", "a+") as status_result:
-        # status_result.writelines(f"{setting}:"+str(retcode)+"\n")
         status_result.writelines(f"{setting}:" + str(retcode))
-
-    # todo: download proton-logs based on setting
     downloaded_log_files_paths = []
     downloaded_log_files_paths = container_file_download(
         './',
         setting,
         *proton_logs_in_container
     )
-
     print(
         f"ci_runner: downloaded_log_files_paths = {downloaded_log_files_paths}"
     )
-
     if run_mode == "github":
         pr_number = os.getenv("GITHUB_REF_NAME", pr_number)
         commit_sha = os.getenv("GITHUB_SHA", commit_sha)
-
-        # s3_helper = S3Helper("https://s3.amazonaws.com")
         if retcode == 0:   
             report_urls = upload_results(
                 s3_helper,
@@ -273,13 +238,11 @@ def ci_runner(
                 commit_sha,
                 *downloaded_log_files_paths,
             )            
-
         for report_url in report_urls:
             report_url = report_url.replace("https://s3.amazonaws.com/", "s3://")
             report_url = report_url.replace("%20", " ")
             print(f"::notice ::Report/Log s3 uri: {report_url}")
             print(f"::notice ::Report/Log download command: aws s3 cp '{report_url}' ./")
-
             proton_log_folder_url = upload_proton_logs(
                 s3_helper,
                 proton_log_folder,
@@ -291,11 +254,7 @@ def ci_runner(
         print("ci_runner: local mode, no report uploaded.")
     return retcode
                    
-
 if __name__ == "__main__":
-    # logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
-    # cur_dir = os.path.dirname(os.path.abspath(__file__))
-    # config_file_path = f"{cur_dir}/test_stream_smoke/configs/config.json"
     parser = argparse.ArgumentParser(description="Run CI in local mode")
     run_mode = "github"
     loop = 1
@@ -310,10 +269,7 @@ if __name__ == "__main__":
     mp_mgr = mp.Manager()
     test_result_shared_list = mp_mgr.list()
     configs = None
-    
-
     parser = ArgumentParser(description="Proton functional tests")
-
     group = parser.add_mutually_exclusive_group(required=False)
     group.add_argument(
         "--github",
@@ -331,128 +287,94 @@ if __name__ == "__main__":
         dest="run_mode",
         help="Run tests in local mode",
     )
-           
     parser.add_argument( 
         "--debug",
         action = "store_true",
         default = False, 
         help="Run tests in debug mode to print debug log, otherwiese info log ouput")
-
     parser.add_argument(
         "-d",
         "--test_folders",
         default=DEFAULT_TEST_FOLDER,
         help="test folders ci_runner will run tests in",
     ) 
-
     parser.add_argument(
         "-q", 
         "--test_suites", 
         default = None, 
         help="Test suite name to be run")  
-
     parser.add_argument(
         "-i", 
         "--id", 
         help="Test suite name to be run")                   
-
     parser.add_argument(
         "--loop",
         default=1,
         type=int,
         help="Num of repeating running tests",
     )
-
     parser.add_argument(
         "-s", 
         "--settings", 
         default = "default", 
         help="settings of proton for testing running on, mapping to the proton settings in proton/tests/stream/test_stream_smoke/configs/config.json")
-    
     parser.add_argument(
         "--cluster_query_route_mode", 
         help="how to run query on cluster, e.g. none_stream_node_first")   
-
     parser.add_argument(
         "--cluster_query_node", 
         help="cluster node name for running queries on, e.g. proton-cluster-node1")
-
     parser.add_argument(
         "--create_stream_shards", 
         help="shards to be created when creating stream, if set settings shards = <create_stream_shards> will be added to all the stream creating statement")
-
     parser.add_argument(
         "--create_stream_replicas", 
         help="replicas to be created when creating stream, if set settings replicas = <create_stream_replicas> will be added to all the stream creating statement")                   
-
     parser.add_argument( 
         "--no_retry",
         action = "store_true",
         default = False, 
         help="Run tests without retry")   
-    
     parser.add_argument(
         "--test_suite_timeout",
         default=DEFAULT_TEST_SUITE_TIMEOUT,
         type=int,
         help="Test suite running time out timer, in seconds, 1200 seconds by default",
     )
-
-   
-    
     parser.add_argument("--tmp", help="Path to tmp dir")
     args = parser.parse_args()
     envs = []
     if args.run_mode:
         run_mode = args.run_mode
         os.environ["PROTON_CI_MODE"] = run_mode 
-
     if args.debug:
         logging_level = "DEBUG"
-
     settings = args.settings.split(",")
     test_folders =args.test_folders
     test_folders_list = args.test_folders.split(",")
-    
-    
-
     if args.test_suites:
         os.environ["PROTON_TEST_SUITES"] = args.test_suites
-    
-    # if args.test_folders:
-    #     os.environ["PROTON_TEST_FOLDERS"] = args.test_folders
-
     if args.loop:
         loop = args.loop
-    
     if args.id:
         os.environ["PROTON_TEST_IDS"] = args.id
-    
     if args.cluster_query_route_mode:
         os.environ["PROTON_CLUSTER_QUERY_ROUTE_MODE"] = args.cluster_query_route_mode
-
     if args.cluster_query_node:
         os.environ["PROTON_CLUSTER_QUERY_NODE"] = args.cluster_query_node
-
     if args.create_stream_shards:
         os.environ["PROTON_CREATE_STREAM_SHARDS"] = args.create_stream_shards
-
     if args.create_stream_replicas:
         os.environ["PROTON_CREATE_STREAM_REPLICAS"] = args.create_stream_replicas
-
     if args.no_retry is not None:
         if args.no_retry is True:
             test_retry = "False"
         else:
             test_retry = "True"
         os.environ["TEST_RETRY"] = test_retry
-    
     if args.test_suite_timeout:
         test_suite_timeout = args.test_suite_timeout
         os.environ["TEST_SUITE_TIMEOUT"] = str(test_suite_timeout)
-
-
-
     console_handler = logging.StreamHandler(sys.stderr)
     console_handler.formatter = formatter
     logger.addHandler(console_handler)
@@ -468,18 +390,15 @@ if __name__ == "__main__":
         logger.setLevel(logging.CRITICAL)
     else:
         logger.setLevel(logging.INFO)
-
     print(
         f"ci_runner starting: run_mode = {run_mode}, loop = {loop}, logging_level={logging_level}, test_retry = {test_retry}, test_suite_timeout = {test_suite_timeout}, test_folders = {test_folders} starts"
     )
-
     if run_mode == "github":
         pr_number = os.getenv("GITHUB_REF_NAME", "0")
         commit_sha = os.getenv("GITHUB_SHA", "0")
     else:
         pr_number = "0"
         commit_sha = "0"
-
     args_vars = vars(args)
     args_dict = {}
     for key, value in args_vars.items():
@@ -487,15 +406,11 @@ if __name__ == "__main__":
             value = 'None'
         arg_dict = {key:value}
         args_dict = {**args_dict, **arg_dict}
-    
     print(f"args_dict = {args_dict}")
-
     with open(config_file_path) as f:
         configs = json.load(f)     
-    
     timeplus_event_stream = configs.get("timeplus_event_stream") #todo: distribute global configs into configs
     timeplus_event_version = configs.get("timeplus_event_version")
-
     #initialize test event fields
     test_command_details = {"test_command_details": {"test_program":__file__.split("/")[-1],"test_paras": args_dict}} #put ci_runner parameters into tag of test_event
     event_id = None
@@ -541,14 +456,11 @@ if __name__ == "__main__":
             traceback.print_exc()
     else:
         print(f"one of TIMEPLUS_API_KEY,TIMEPLUS_ADDRESS,TIMEPLUS_WORKSPACE is not found in ENV")   
-            
     logger.info(f"Check proton_python_driver and install...")
     proton_python_driver_install()
-
     if "test_production_compatibility" in test_folders_list: #todo: hardcode now, refactor later to have a system_test_runner to run all pytests folders like test_production_compatibility
         settings = ["default"]
         docker_compose_file_path = f"{cur_dir}/test_production_compatibility/configs/docker-compose.yaml"
-
     if run_mode == "local":
         env_docker_compose_res = True
         logger.info(f"Bypass docker compose up.")
@@ -556,10 +468,8 @@ if __name__ == "__main__":
         env_docker_compose_res = compose_up(docker_compose_file_path)
         logger.info(f"docker compose up...")
     logger.debug(f"env_docker_compose_res: {env_docker_compose_res}")
-
     if not env_docker_compose_res:
         raise Exception("Env docker compose up failure.")
-    
     if settings == []:
         #settings = ["nativelog"]
         settings = ["default"] 
@@ -580,22 +490,11 @@ if __name__ == "__main__":
         time.sleep(5)
     for proc in procs:
         proc.join()
-
     ci_runner_end = datetime.datetime.now()
-    
     ci_runner_duration = ci_runner_end - ci_runner_start
-
     logger.info(
         f"ci_runner end: run_mode = {run_mode}, loop = {loop}, logging_level={logging_level}, test_suite_timeout = {test_suite_timeout}, ci_runner_duration = {ci_runner_duration.seconds} seconds, ends"
     )
-
-
-
-    # while i < loop:
-    #    ci_runner(cur_dir, run_mode, logging_level = logging_level)
-    #    i += 1
-
-    
     test_result_flag = 1
     detailed_summary = []
     for item in test_result_shared_list: #retcode: {'proton_setting': 'default', 'retcode': <ExitCode.TESTS_FAILED: 1>}:
@@ -604,12 +503,10 @@ if __name__ == "__main__":
         if "OK" not in retcode_str:
             test_result_flag = test_result_flag * 0
         detailed_summary.append({setting:retcode_str})
-    
     if test_result_flag:
         test_result_str = "success"
     else:
         test_result_str = "failed"
-    
     logger.info(f"test_result_str = {test_result_str}")           
     test_result = {"test_result": test_result_str, "detailed_summary": detailed_summary}
     if api_address is not None and api_key is not None:
@@ -619,8 +516,6 @@ if __name__ == "__main__":
             test_event_record_end = EventRecord.create(None, test_event_end, test_event_tag, timeplus_event_version)
             print(f"test_event_end = {test_event_end}")
             test_event_record_end.write(timeplus_env, timeplus_event_stream) 
-
-
         except(BaseException) as error:
             logger.error(f"timeplus event write exception: {error}")
             traceback.print_exc()  
