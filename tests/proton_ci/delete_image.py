d9675d92e9e906cc215dc23bfa7d6d812d53641f
Leo Cai <leo.cai@timeplus.io>
2023-10-09 18:08:03 +0800

    manually delete image version using tag

--- tests/proton_ci/delete_image.py
+++ tests/proton_ci/delete_image.py
@@ -1,35 +1,33 @@
 import requests
 import os
+from env_helper import GH_PERSONAL_ACCESS_TOKEN
 
-
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-USERTYPE = "orgs"
-USERNAME = "timeplus-io"
-PACKAGE_TYPE = "container"
-PACKAGE_NAME = "proton"
 PROTON_VERSION = os.getenv("PROTON_VERSION")
 HEADERS = {
     "Accept": "application/vnd.github+json",
-    "Authorization": f"Bearer {GITHUB_TOKEN}",
+    "Authorization": f"Bearer {GH_PERSONAL_ACCESS_TOKEN}",
     "X-GitHub-Api-Version": "2022-11-28",
 }
+
 package_versions = requests.get(
-    f"https://api.github.com/{USERTYPE}/{USERNAME}/packages/{PACKAGE_TYPE}/{PACKAGE_NAME}/versions",
+    f"https://api.github.com/orgs/timeplus-io/packages/container/proton/versions",
     headers=HEADERS,
 ).json()
-version_id = -1
+
+version_id = None
 for package_version in package_versions:
     if PROTON_VERSION in package_version["metadata"]["container"]["tags"]:
         version_id = package_version["id"]
-if version_id != -1:
+
+if version_id is not None:
     delete_result = requests.delete(
-        f"https://api.github.com/{USERTYPE}/{USERNAME}/packages/{PACKAGE_TYPE}/{PACKAGE_NAME}/versions/{version_id}",
+        f"https://api.github.com/orgs/timeplus-io/packages/container/proton/versions/{version_id}",
         headers=HEADERS,
     )
+
     if delete_result.status_code == 204:
         print(f"delete {PROTON_VERSION} success")
     else:
-        print(f"delete error, result:{delete_result.text}")
+        print(f"delete error, result: {delete_result.text}")
 else:
     print("image not found")
-
