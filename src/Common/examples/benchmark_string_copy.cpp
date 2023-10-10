48294d1da9787ec04dee6c195ff79564b056ecb7
yokofly <haohang.shi@timeplus.io>
2023-10-10 02:29:34 -0700

    submodule change: introduce google bench (disable by default)

---
+++ src/Common/examples/benchmark_string_copy.cpp
@@ -0,0 +1,26 @@
+#include <benchmark/benchmark.h>
+
+/// example code from: https://quick-bench.com/
+
+static void StringCreation(benchmark::State& state) {
+  /// Code inside this loop is measured repeatedly
+  for (auto _ : state) {
+    std::string created_string("hello");
+    /// Make sure the variable is not optimized away by compiler
+    benchmark::DoNotOptimize(created_string);
+  }
+}
+/// Register the function as a benchmark
+BENCHMARK(StringCreation);
+
+static void StringCopy(benchmark::State& state) {
+  /// Code before the loop is not measured
+  std::string x = "hello";
+  for (auto _ : state) {
+    std::string copy(x);
+  }
+}
+
+BENCHMARK(StringCopy);
+
+BENCHMARK_MAIN();
