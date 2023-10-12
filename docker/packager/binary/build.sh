af8150ab3abc10d33f1ab6c054579d83061a9be4
yokofly <haohang.shi@timeplus.io>
2023-10-11 21:09:10 -0700

    fix build warn in my local docker build. ``` # fatal: detected dubious ownership in repository at '/build' ```

--- docker/packager/binary/build.sh
+++ docker/packager/binary/build.sh
@@ -2,6 +2,11 @@
 
 set -x -e
 
+# fatal: detected dubious ownership in repository at '/build'
+# To add an exception for this directory, call:
+# git config --global --add safe.directory /build
+git config --global --add safe.directory /build
+
 mkdir -p build/cmake/toolchain/darwin-x86_64
 tar xJf MacOSX11.0.sdk.tar.xz -C build/cmake/toolchain/darwin-x86_64 --strip-components=1
 ln -sf darwin-x86_64 build/cmake/toolchain/darwin-aarch64
