e53c89dfcaa2b225edecee2df4438aa69b594b1e
Ken Chen <zlchen.ken@gmail.com>
2023-11-22 10:40:31 -0800

    Switched to SortedLookupContainer and only enable alignment when join threshold is not zero

--- src/Interpreters/Streaming/RowRefs.h
+++ src/Interpreters/Streaming/RowRefs.h
@@ -6,6 +6,7 @@
 #include <Interpreters/RowRefs.h>
 #include <Interpreters/Streaming/RangeAsofJoinContext.h>
 #include <Interpreters/Streaming/RefCountBlockList.h>
+#include <Interpreters/Streaming/SortedLookupContainer.h>
 #include <Interpreters/Streaming/joinSerder_fwd.h>
 #include <Interpreters/Streaming/joinTuple.h>
 #include <base/sort.h>
@@ -280,87 +281,6 @@ struct RowRefListMultipleRef
 template <typename DataBlock>
 using RowRefListMultipleRefPtr = std::unique_ptr<RowRefListMultipleRef<DataBlock>>;
 
-template <typename RowRefDataBlock, typename TEntry>
-class SortedLookupVector
-{
-public:
-    using Base = std::vector<TEntry>;
-
-    void insert(TEntry entry, bool ascending, size_t max_size)
-    {
-        /// Happy and perf path
-        if (ascending)
-        {
-            if (array.empty() || less(array.back(), entry)) /// Very likely
-            {
-                array.push_back(std::move(entry));
-                eraseIfExceedingMaxSize(max_size, ascending);
-                return;
-            }
-        }
-        else
-        {
-            if (array.empty() || less(array.front(), entry)) /// Very likely
-            {
-                array.insert(array.begin(), std::move(entry));
-                eraseIfExceedingMaxSize(max_size, ascending);
-                return;
-            }
-        }
-
-        auto it = std::lower_bound(array.begin(), array.end(), entry, (ascending ? less : greater));
-        array.insert(it, entry);
-        eraseIfExceedingMaxSize(max_size, ascending);
-    }
-
-    const RowRefDataBlock * upperBound(const TEntry & k, bool ascending)
-    {
-        auto it = std::upper_bound(array.cbegin(), array.cend(), k, (ascending ? less : greater));
-        if (it != array.cend())
-            return &(it->row_ref);
-        return nullptr;
-    }
-
-    const RowRefDataBlock * lowerBound(const TEntry & k, bool ascending)
-    {
-        auto it = std::lower_bound(array.cbegin(), array.cend(), k, (ascending ? less : greater));
-        if (it != array.cend())
-            return &(it->row_ref);
-        return nullptr;
-    }
-
-    using iterator = typename Base::iterator;
-    using const_iterator = typename Base::const_iterator;
-
-    iterator begin() { return array.begin(); }
-    iterator end() { return array.end(); }
-
-    size_t size() const { return array.size(); }
-    void resize(size_t s) { array.resize(s); }
-
-    const_iterator begin() const { return array.begin(); }
-    const_iterator end() const { return array.end(); }
-
-private:
-    inline void eraseIfExceedingMaxSize(size_t max_size, bool ascending)
-    {
-        if (array.size() > max_size)
-        {
-            if (ascending)
-                array.erase(array.begin());
-            else
-                array.pop_back();
-        }
-    }
-
-private:
-    Base array;
-
-    static bool less(const TEntry & a, const TEntry & b) { return a.asof_value < b.asof_value; }
-
-    static bool greater(const TEntry & a, const TEntry & b) { return a.asof_value > b.asof_value; }
-};
-
 template <typename DataBlock>
 class AsofRowRefs
 {
@@ -369,7 +289,7 @@ public:
     template <typename T>
     struct Entry
     {
-        using LookupType = SortedLookupVector<RowRefDataBlock, Entry<T>>;
+        using LookupType = SortedLookupContainer<RowRefDataBlock, Entry<T>>;
         using LookupPtr = std::unique_ptr<LookupType>;
 
         T asof_value;
