9ca5faa38980d36ac1796e7dcf92c8cdb522eacd
Jove Zhong <jove@timeplus.io>
2023-12-08 22:09:50 -0800

    avoid hardcode IP and stream name

--- examples/awesome-sensor-logger/src/index.routes.ts
+++ examples/awesome-sensor-logger/src/index.routes.ts
@@ -1,10 +1,11 @@
 import { routes } from '@stricjs/app';
 
 export function main() {
+    const protonIngestEndpoint = `http://${process.env.HOST}:3218/proton/v1/ingest/streams/${process.env.STREAM}`;
     return routes()
         .post('/', c => {
             return c.text().then(a => {
-                return fetch("http://localhost:3218/proton/v1/ingest/streams/phone", {
+                return fetch(protonIngestEndpoint, {
                     method: "POST",
                     body: `{"columns": ["raw"],"data": [["${a.replaceAll('"', '\\\"')}"]]}`,
                     headers: { "Content-Type": "application/json" },
