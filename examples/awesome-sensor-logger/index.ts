fe553b700565a5f90579a4c48bbdc0da9418b16c
Jove Zhong <jove@timeplus.io>
2023-12-08 21:03:51 -0800

    a working version

--- examples/awesome-sensor-logger/index.ts
+++ examples/awesome-sensor-logger/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { Router } from '@stricjs/router';
+
+export default new Router()
+    .get('/a', ()=>new Response('test'))
+    .post('/', c => {
+        console.log('got post')
+        return c.text().then(a => {
+            return fetch("http://localhost:3218/proton/v1/ingest/streams/phone", {
+                method: "POST",
+                body:`{"columns": ["raw"],"data": [["${a.replaceAll('"','\\\"')}"]]}`,
+                headers: { "Content-Type": "application/json" },
+            }).then(protonResp=>new Response('status code '+protonResp.status));
+        });
+    });
