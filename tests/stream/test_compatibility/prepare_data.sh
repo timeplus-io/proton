d6cff179c82e42a265bad492149eb7d922a9eea7
Leo Cai <143980630+leo-cai-timeplus@users.noreply.github.com>
2023-12-12 13:15:00 +0800

    Feature/issue 396 refine ci for compatibility test

--- tests/stream/test_compatibility/prepare_data.sh
+++ tests/stream/test_compatibility/prepare_data.sh
@@ -1,7 +1,8 @@
 docker-compose -f test_compatibility/docker-compose.yaml up -d
-docker exec -it proton-server proton client -nm -q "create stream example(x int);"
-docker exec -it proton-server proton client -nm -q "insert into example (x) values (1)(2)(3);"
-docker exec -it redpanda rpk topic create example_topic
-docker exec -it proton-server proton client -nm -q "create external stream example_external (x int) settings type='kafka', brokers='stream-store:9092',topic='example_topic',data_format = 'JSONEachRow';"
-docker exec -it proton-server proton client -nm -q "insert into example_external (x) values (2)(4)(6);"
+sleep 5
+docker exec proton-server proton client -nm -q "create stream example(x int);"
+docker exec proton-server proton client -nm -q "insert into example (x) values (1)(2)(3);"
+docker exec redpanda rpk topic create example_topic
+docker exec proton-server proton client -nm -q "create external stream example_external (x int) settings type='kafka', brokers='stream-store:9092',topic='example_topic',data_format = 'JSONEachRow';"
+docker exec proton-server proton client -nm -q "insert into example_external (x) values (2)(4)(6);"
 docker-compose -f test_compatibility/docker-compose.yaml down
