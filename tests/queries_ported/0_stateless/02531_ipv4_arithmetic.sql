f27e79f5f7ee5d0d7c3706b813ab57650d6f4240
lijianan <jianan.li@timeplus.com>
2023-12-12 13:58:49 +0800

    add more test

--- tests/queries_ported/0_stateless/02531_ipv4_arithmetic.sql
+++ tests/queries_ported/0_stateless/02531_ipv4_arithmetic.sql
@@ -1,1 +1,5 @@
 SELECT number, ip, ip % number FROM (SELECT number, to_ipv4('1.2.3.4') as ip FROM numbers(10, 20));
+SELECT bit_shift_left(to_ipv4('192.168.64.15'), 24);
+SELECT bit_shift_right(to_ipv4('192.168.64.15'), 24);
+SELECT bit_and(to_ipv4('1.0.78.44'), to_ipv4('1.0.78.45'));
+SELECT bit_or(to_ipv4('1.0.78.44'), to_ipv4('1.0.78.45'));
