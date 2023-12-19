3a485f33872b236809e5240273f43a33070e0e1e
lijianan <jianan.li@timeplus.com>
2023-12-19 18:33:20 +0800

    fix combination like count_if and add test

--- src/Interpreters/Streaming/SubstituteStreamingFunction.cpp
+++ src/Interpreters/Streaming/SubstituteStreamingFunction.cpp
@@ -125,7 +125,8 @@ void StreamingFunctionData::visit(DB::ASTFunction & func, DB::ASTPtr)
 
     if (streaming)
     {
-        auto iter = func_map.find(Poco::toLower(func.name));
+        auto func_name_lower = Poco::toLower(func.name);
+        auto iter = func_map.find(func_name_lower);
         if (iter != func_map.end())
             return substitueFunction(func, iter->second);
 
@@ -133,7 +134,7 @@ void StreamingFunctionData::visit(DB::ASTFunction & func, DB::ASTPtr)
         {
             /// Whether the function support 'retract' for changelog, also return the alias name of
             /// function used in rewritten query
-            auto func_alias_name = supportChangelog(Poco::toLower(func.name));
+            auto func_alias_name = supportChangelog(func_name_lower);
             if (func_alias_name.has_value())
             {
                 if (!func_alias_name->empty())
--- tests/queries_ported/0_stateless/02815_function_name_case_insensitive.sql
+++ tests/queries_ported/0_stateless/02815_function_name_case_insensitive.sql
@@ -1,9 +1,17 @@
+SET query_mode = 'table';
 drop stream if exists test_02815;
-create stream if not exists test_02815(id int, value int) engine=Memory;
+create stream if not exists test_02815(id int, value int);
 insert into test_02815(id, value) values (1, 4);
-insert into test_02815(id, value) values (2, 3);
+insert into test_02815(id, value) values (2, 4);
 insert into test_02815(id, value) values (1, 2);
 insert into test_02815(id, value) values (2, 7);
--- all function are case insensitive
-select COUNT(*), MAX(value), MIN(value), AVG(value), SUM(value), VAR_SAMP(value), to_String(id) from test_02815 group by id;
+insert into test_02815(id, value) values (3, 2);
+insert into test_02815(id, value) values (3, 10);
+select sleep(3);
+select '---- normal aggregate functions ----';
+select id, COUNT(*), MAX(value), MIN(value), AVG(value), SUM(value), VAR_SAMP(value), to_String(id) from test_02815 group by id;
+
+select '---- combination aggrefation functions ----';
+
+select id, COUNT_IF(value > 3), SUM_DISTINCT(value) from test_02815 group by id;
 drop stream if exists test_02815;
