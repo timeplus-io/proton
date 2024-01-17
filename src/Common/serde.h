cbcd69de77b13407934c13b49fcf5d8d00563b33
Lisen 杨 <li.yang@timeplus.io>
2024-01-17 15:21:16 +0800

    fix comments 2

--- src/Common/serde.h
+++ src/Common/serde.h
@@ -7,37 +7,24 @@
 namespace DB
 {
 /// REQUIRES: The object must support versioned serialization/deserialization
-template <typename S, typename WB>
-concept VersionedSerializable = requires(const S & s, WB && wb, VersionType version) { s.serialize(wb, version); };
+template <typename S, typename WB, typename... Args>
+concept VersionedSerializable
+    = requires(const S & s, WB & wb, VersionType version, Args &&... args) { s.serialize(wb, version, std::forward<Args>(args)...); };
 
-template <typename S, typename RB>
-concept VersionedDeserializable = requires(S && s, RB && rb, VersionType version) { s.deserialize(rb, version); };
+template <typename S, typename RB, typename... Args>
+concept VersionedDeserializable
+    = requires(S & s, RB & rb, VersionType version, Args &&... args) { s.deserialize(rb, version, std::forward<Args>(args)...); };
 
-template <typename T>
-concept HasGetVersionFromRevision = requires(T && a) {
-    {
-        a.getVersionFromRevision(ProtonRevision::getVersionRevision())
-    } -> std::convertible_to<VersionType>;
-};
-
-template <typename WB, VersionedSerializable<WB> S>
-void ALWAYS_INLINE serialize(const S & s, WB && wb)
+template <typename WB, typename... Args, VersionedSerializable<WB, Args...> S>
+void ALWAYS_INLINE serialize(const S & s, WB & wb, VersionType version, Args &&... args)
 {
-    /// By default use `Revision` for version management, unless it has its own versions
-    if constexpr (HasGetVersionFromRevision<S>)
-        s.serialize(wb, static_cast<VersionType>(s.getVersionFromRevision(ProtonRevision::getVersionRevision())));
-    else
-        s.serialize(wb, static_cast<VersionType>(ProtonRevision::getVersionRevision()));
+    s.serialize(wb, version, std::forward<Args>(args)...);
 }
 
-template <typename RB, VersionedDeserializable<RB> S>
-void ALWAYS_INLINE deserialize(S & s, RB && rb)
+template <typename RB, typename... Args, VersionedDeserializable<RB, Args...> S>
+void ALWAYS_INLINE deserialize(S & s, RB & rb, VersionType version, Args &&... args)
 {
-    /// By default use `Revision` for version management, unless it has its own versions
-    if constexpr (HasGetVersionFromRevision<S>)
-        s.deserialize(rb, static_cast<VersionType>(s.getVersionFromRevision(ProtonRevision::getVersionRevision())));
-    else
-        s.deserialize(rb, static_cast<VersionType>(ProtonRevision::getVersionRevision()));
+    s.deserialize(rb, version, std::forward<Args>(args)...);
 }
 
 /// macro tag to indicate the data members or struct or class will
