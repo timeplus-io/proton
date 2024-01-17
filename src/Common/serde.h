598458bfb8b79a0344cf1f1b69093108f401e72c
Lisen 杨 <li.yang@timeplus.io>
2024-01-17 11:45:17 +0800

    fix comments

--- src/Common/serde.h
+++ src/Common/serde.h
@@ -21,37 +21,23 @@ concept HasGetVersionFromRevision = requires(T && a) {
 };
 
 template <typename WB, VersionedSerializable<WB> S>
-void ALWAYS_INLINE serialize(const S & s, WB && wb, std::optional<VersionType> version = std::nullopt)
+void ALWAYS_INLINE serialize(const S & s, WB && wb)
 {
-    if (version.has_value())
-    {
-        s.serialize(wb, version.value());
-    }
+    /// By default use `Revision` for version management, unless it has its own versions
+    if constexpr (HasGetVersionFromRevision<S>)
+        s.serialize(wb, static_cast<VersionType>(s.getVersionFromRevision(ProtonRevision::getVersionRevision())));
     else
-    {
-        /// When version is not required, by default use `Revision` for version management, unless it has its own versions
-        if constexpr (HasGetVersionFromRevision<S>)
-            s.serialize(wb, static_cast<VersionType>(s.getVersionFromRevision(ProtonRevision::getVersionRevision())));
-        else
-            s.serialize(wb, static_cast<VersionType>(ProtonRevision::getVersionRevision()));
-    }
+        s.serialize(wb, static_cast<VersionType>(ProtonRevision::getVersionRevision()));
 }
 
 template <typename RB, VersionedDeserializable<RB> S>
-void ALWAYS_INLINE deserialize(S & s, RB && rb, std::optional<VersionType> version = std::nullopt)
+void ALWAYS_INLINE deserialize(S & s, RB && rb)
 {
-    if (version.has_value())
-    {
-        s.deserialize(rb, version.value());
-    }
+    /// By default use `Revision` for version management, unless it has its own versions
+    if constexpr (HasGetVersionFromRevision<S>)
+        s.deserialize(rb, static_cast<VersionType>(s.getVersionFromRevision(ProtonRevision::getVersionRevision())));
     else
-    {
-        /// When version is not required, by default use `Revision` for version management, unless it has its own versions
-        if constexpr (HasGetVersionFromRevision<S>)
-            s.deserialize(rb, static_cast<VersionType>(s.getVersionFromRevision(ProtonRevision::getVersionRevision())));
-        else
-            s.deserialize(rb, static_cast<VersionType>(ProtonRevision::getVersionRevision()));
-    }
+        s.deserialize(rb, static_cast<VersionType>(ProtonRevision::getVersionRevision()));
 }
 
 /// macro tag to indicate the data members or struct or class will
