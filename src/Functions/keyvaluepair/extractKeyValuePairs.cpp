48461584b30f6dd8dbef16fd2e7b54a040190b1a
yokofly <haohang.shi@timeplus.io>
2024-01-25 02:02:48 -0800

    Add setting to limit the max number of pairs produced by extractKeyVa ... porting clickhouse/clickhouse#49836 on May 15, 2023

--- src/Functions/keyvaluepair/extractKeyValuePairs.cpp
+++ src/Functions/keyvaluepair/extractKeyValuePairs.cpp
@@ -7,6 +7,8 @@
 #include <DataTypes/DataTypeMap.h>
 #include <DataTypes/DataTypeString.h>
 
+#include <Interpreters/Context.h>
+
 #include <Functions/keyvaluepair/impl/KeyValuePairExtractor.h>
 #include <Functions/keyvaluepair/impl/KeyValuePairExtractorBuilder.h>
 #include <Functions/keyvaluepair/ArgumentExtractor.h>
@@ -41,6 +43,13 @@ class ExtractKeyValuePairs : public IFunction
             builder.withQuotingCharacter(parsed_arguments.quoting_character.value());
         }
 
+        bool is_number_of_pairs_unlimited = context->getSettingsRef().extract_kvp_max_pairs_per_row == 0;
+
+        if (!is_number_of_pairs_unlimited)
+        {
+            builder.withMaxNumberOfPairs(context->getSettingsRef().extract_kvp_max_pairs_per_row);
+        }
+
         return builder.build();
     }
 
@@ -73,7 +82,7 @@ class ExtractKeyValuePairs : public IFunction
     }
 
 public:
-    ExtractKeyValuePairs() = default;
+    explicit ExtractKeyValuePairs(ContextPtr context_) : context(context_) {}
 
     static constexpr auto name = Name::name;
 
@@ -82,9 +91,9 @@ public:
         return name;
     }
 
-    static FunctionPtr create(ContextPtr)
+    static FunctionPtr create(ContextPtr context)
     {
-        return std::make_shared<ExtractKeyValuePairs>();
+        return std::make_shared<ExtractKeyValuePairs>(context);
     }
 
     ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t) const override
@@ -120,6 +129,9 @@ public:
     {
         return {1, 2, 3, 4};
     }
+
+private:
+    ContextPtr context;
 };
 
 struct NameExtractKeyValuePairs
@@ -137,44 +149,63 @@ REGISTER_FUNCTION(ExtractKeyValuePairs)
     factory.registerFunction<ExtractKeyValuePairs<NameExtractKeyValuePairs, false>>(
         Documentation(
             R"(Extracts key-value pairs from any string. The string does not need to be 100% structured in a key value pair format;
+
             It can contain noise (e.g. log files). The key-value pair format to be interpreted should be specified via function arguments.
+
             A key-value pair consists of a key followed by a `key_value_delimiter` and a value. Quoted keys and values are also supported. Key value pairs must be separated by pair delimiters.
+
             **Syntax**
             ``` sql
             extract_key_value_pairs(data, [key_value_delimiter], [pair_delimiter], [quoting_character])
             ```
+
             **Arguments**
             - `data` - String to extract key-value pairs from. [String](../../sql-reference/data-types/string.md) or [FixedString](../../sql-reference/data-types/fixedstring.md).
             - `key_value_delimiter` - Character to be used as delimiter between the key and the value. Defaults to `:`. [String](../../sql-reference/data-types/string.md) or [FixedString](../../sql-reference/data-types/fixedstring.md).
             - `pair_delimiters` - Set of character to be used as delimiters between pairs. Defaults to `\space`, `,` and `;`. [String](../../sql-reference/data-types/string.md) or [FixedString](../../sql-reference/data-types/fixedstring.md).
             - `quoting_character` - Character to be used as quoting character. Defaults to `"`. [String](../../sql-reference/data-types/string.md) or [FixedString](../../sql-reference/data-types/fixedstring.md).
+
             **Returned values**
             - The extracted key-value pairs in a Map(String, String).
+
             **Examples**
+
             Query:
+
             **Simple case**
             ``` sql
             arthur :) select extract_key_value_pairs('name:neymar, age:31 team:psg,nationality:brazil') as kv
+
             SELECT extract_key_value_pairs('name:neymar, age:31 team:psg,nationality:brazil') as kv
+
             Query id: f9e0ca6f-3178-4ee2-aa2c-a5517abb9cee
+
             ┌─kv──────────────────────────────────────────────────────────────────────┐
             │ {'name':'neymar','age':'31','team':'psg','nationality':'brazil'}        │
             └─────────────────────────────────────────────────────────────────────────┘
             ```
+
             **Single quote as quoting character**
             ``` sql
             arthur :) select extract_key_value_pairs('name:\'neymar\';\'age\':31;team:psg;nationality:brazil,last_key:last_value', ':', ';,', '\'') as kv
+
             SELECT extract_key_value_pairs('name:\'neymar\';\'age\':31;team:psg;nationality:brazil,last_key:last_value', ':', ';,', '\'') as kv
+
             Query id: 0e22bf6b-9844-414a-99dc-32bf647abd5e
+
             ┌─kv───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
             │ {'name':'neymar','age':'31','team':'psg','nationality':'brazil','last_key':'last_value'}                                 │
             └──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
             ```
+
             **Escape sequences without escape sequences support**
             ``` sql
             arthur :) select extract_key_value_pairs('age:a\\x0A\\n\\0') as kv
+
             SELECT extract_key_value_pairs('age:a\\x0A\\n\\0') AS kv
+
             Query id: e9fd26ee-b41f-4a11-b17f-25af6fd5d356
+
             ┌─kv────────────────────┐
             │ {'age':'a\\x0A\\n\\0'} │
             └───────────────────────┘
@@ -184,19 +215,26 @@ REGISTER_FUNCTION(ExtractKeyValuePairs)
     factory.registerFunction<ExtractKeyValuePairs<NameExtractKeyValuePairsWithEscaping, true>>(
         Documentation(
             R"(Same as `extract_key_value_pairs` but with escaping support.
+
             Escape sequences supported: `\x`, `\N`, `\a`, `\b`, `\e`, `\f`, `\n`, `\r`, `\t`, `\v` and `\0`.
             Non standard escape sequences are returned as it is (including the backslash) unless they are one of the following:
             `\\`, `'`, `"`, `backtick`, `/`, `=` or ASCII control characters (c <= 31).
+
             This function will satisfy the use case where pre-escaping and post-escaping are not suitable. For instance, consider the following
             input string: `a: "aaaa\"bbb"`. The expected output is: `a: aaaa\"bbbb`.
             - Pre-escaping: Pre-escaping it will output: `a: "aaaa"bbb"` and `extract_key_value_pairs` will then output: `a: aaaa`
             - Post-escaping: `extract_key_value_pairs` will output `a: aaaa\` and post-escaping will keep it as it is.
+
             Leading escape sequences will be skipped in keys and will be considered invalid for values.
+
             **Escape sequences with escape sequence support turned on**
             ``` sql
             arthur :) select extract_key_value_pairs_with_escaping('age:a\\x0A\\n\\0') as kv
+
             SELECT extract_key_value_pairs_with_escaping('age:a\\x0A\\n\\0') AS kv
+
             Query id: 44c114f0-5658-4c75-ab87-4574de3a1645
+
             ┌─kv───────────────┐
             │ {'age':'a\n\n\0'} │
             └──────────────────┘
