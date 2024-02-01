305770b62ea9368ef8a1636b16a4d7931216a1c8
Gimi Liang <gimi.liang@timeplus.io>
2024-01-31 20:33:52 -0800

    fixed UT

--- src/Storages/ExternalStream/tests/gtest_settings.cpp
+++ src/Storages/ExternalStream/tests/gtest_settings.cpp
@@ -5,19 +5,15 @@
 
 TEST(ExternalStreamSettings, getFormatSettings)
 {
-    auto & context = getMutableContext();
-    auto original_value = context.context->getSettingsRef().format_csv_delimiter;
-    context.context->setSetting("format_csv_delimiter", '/');
+    auto context = DB::Context::createCopy(getContext().context);
+    context->setSetting("format_csv_delimiter", DB::Field("/"));
 
     DB::ExternalStreamSettings ex_stream_settings{};
     ex_stream_settings.format_csv_delimiter = ';';
     ex_stream_settings.bool_true_representation = "yes";
 
-    auto settings = ex_stream_settings.getFormatSettings(context.context);
+    auto settings = ex_stream_settings.getFormatSettings(context);
     EXPECT_EQ(settings.csv.delimiter, '/'); /// from settings_for_context
     EXPECT_EQ(settings.bool_true_representation, "yes"); /// from ex_stream_settings
     EXPECT_EQ(settings.bool_false_representation, "false"); /// default value
-
-    /// restore
-    context.context->setSetting("format_csv_delimiter", original_value.value);
 }
