bfc7545d9b232d3e8edda2240fa55db170244687
yoko <haohang.shi@timeplus.io>
2024-02-25 23:19:07 -0800

    Fix assertion in full soring merge join porting clickhouse/clickhouse#39014 on Jul 8, 2022

--- tests/queries_ported/0_stateless/02277_full_sort_join_misc.sql
+++ tests/queries_ported/0_stateless/02277_full_sort_join_misc.sql
@@ -1,16 +1,16 @@
 SET join_algorithm = 'full_sorting_merge';
 
-SELECT * FROM (SELECT 1 as key) AS t1 JOIN (SELECT 1 as key) t2 ON t1.key = t2.key ORDER BY key;
+SELECT * FROM (SELECT 1 as key) AS t1 JOIN (SELECT 1 as key) AS t2 ON t1.key = t2.key ORDER BY key;
 
-SELECT * FROM (SELECT 1 as key) AS t1 JOIN (SELECT 1 as key) t2 USING key ORDER BY key;
+SELECT * FROM (SELECT 1 as key) AS t1 JOIN (SELECT 1 as key) AS t2 USING key ORDER BY key;
 
-SELECT * FROM (SELECT 1 :: uint32 as key) AS t1 FULL JOIN (SELECT 1 :: nullable(uint32) as key) t2 USING (key) ORDER BY key;
+SELECT * FROM (SELECT 1 :: uint32 as key) AS t1 FULL JOIN (SELECT 1 :: nullable(uint32) as key) AS t2 USING (key) ORDER BY key;
 
-SELECT * FROM (SELECT 1 :: uint32 as key) AS t1 FULL JOIN (SELECT NULL :: nullable(uint32) as key) t2 USING (key) ORDER BY key;
+SELECT * FROM (SELECT 1 :: uint32 as key) AS t1 FULL JOIN (SELECT NULL :: nullable(uint32) as key) AS t2 USING (key) ORDER BY key;
 
-SELECT * FROM (SELECT 1 :: int32 as key) AS t1 JOIN (SELECT 1 :: uint32 as key) t2 ON t1.key = t2.key ORDER BY key;
+SELECT * FROM (SELECT 1 :: int32 as key) AS t1 JOIN (SELECT 1 :: uint32 as key) AS t2 ON t1.key = t2.key ORDER BY key;
 
-SELECT * FROM (SELECT -1 :: nullable(int32) as key) AS t1 FULL JOIN (SELECT 4294967295 :: uint32 as key) t2 ON t1.key = t2.key ORDER BY key;
+SELECT * FROM (SELECT -1 :: nullable(int32) as key) AS t1 FULL JOIN (SELECT 4294967295 :: uint32 as key) AS t2 ON t1.key = t2.key ORDER BY key;
 
 SELECT * FROM (SELECT 'a' :: low_cardinality(string) AS key) AS t1 JOIN (SELECT 'a' :: string AS key) AS t2 ON t1.key = t2.key ORDER BY key;
 
