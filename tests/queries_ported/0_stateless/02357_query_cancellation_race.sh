2cdc30a9214f7961a21df37f33fcf031fac07c8b
haohang <haohang.shi@timeplus.io>
2024-04-27 13:53:38 +0800

    Fix data race in CompletedPipelineExecutor. (#681)
    porting https://github.com/ClickHouse/ClickHouse/pull/39132/files  on Jul 13, 2022

--- tests/queries_ported/0_stateless/02357_query_cancellation_race.sh
+++ tests/queries_ported/0_stateless/02357_query_cancellation_race.sh
@@ -7,3 +7,6 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 $CLICKHOUSE_CLIENT -q "create stream tab (x uint64, y string) engine = MergeTree order by x"
 for _ in $(seq 1 100); do timeout -s 2 0.05 $CLICKHOUSE_CLIENT --interactive_delay 1000 -q "insert into tab select number, to_string(number) from system.numbers" || true; done
+
+$CLICKHOUSE_CLIENT -q "create stream tap (x uint64, y string)"
+for _ in $(seq 1 100); do timeout -s 2 0.05 $CLICKHOUSE_CLIENT --interactive_delay 1000 -q "insert into tap(x, y) select number, to_string(number) from system.numbers" || true; done
