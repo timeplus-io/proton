84ab07ea07952db1004b0f144c91563ba049db58
Ken Chen <zlchen.ken@gmail.com>
2024-05-15 23:10:19 -0700

    Feature/issue 712 timeplusd endpoints (#713)
    Co-authored-by: yoko <haohang.shi@timeplus.io>

--- src/Server/RestRouterHandlers/RestRouterFactory.h
+++ src/Server/RestRouterHandlers/RestRouterFactory.h
@@ -4,7 +4,6 @@
 #include "ClusterInfoHandler.h"
 #include "ColumnRestRouterHandler.h"
 #include "DatabaseRestRouterHandler.h"
-#include "StorageInfoHandler.h"
 #include "ExternalStreamRestRouterHandler.h"
 #include "IngestRawStoreHandler.h"
 #include "IngestRestRouterHandler.h"
@@ -17,9 +16,10 @@
 #include "SQLAnalyzerRestRouterHandler.h"
 #include "SQLFormatHandler.h"
 #include "SearchHandler.h"
+#include "StorageInfoHandler.h"
+#include "SystemCommandHandler.h"
 #include "TabularTableRestRouterHandler.h"
 #include "UDFHandler.h"
-#include "SystemCommandHandler.h"
 
 #include <re2/re2.h>
 #include <Common/escapeForFileName.h>
@@ -32,7 +32,7 @@ using CompiledRegexPtr = std::shared_ptr<const re2::RE2>;
 
 namespace ErrorCodes
 {
-    extern const int CANNOT_COMPILE_REGEXP;
+extern const int CANNOT_COMPILE_REGEXP;
 }
 
 class RestRouterFactory final
@@ -49,171 +49,180 @@ public:
         constexpr auto * stream_name_regex = "(?P<stream>[_%\\.\\-\\w]+)";
 
         auto & factory = RestRouterFactory::instance();
-        factory.registerRouterHandler(
-            fmt::format("/proton/v1/ingest/streams/{}(\\?mode=\\w+){{0,1}}", stream_name_regex),
-            "POST",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<IngestRestRouterHandler>(query_context);
-            });
+        for (const auto * prefix : {"proton", "timeplusd"})
+        {
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/ingest/streams/{}(\\?mode=\\w+){{0,1}}", prefix, stream_name_regex),
+                "POST",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<IngestRestRouterHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            fmt::format("/proton/v1/ingest/rawstores/{}(\\?mode=\\w+){{0,1}}", stream_name_regex),
-            "POST",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<IngestRawStoreHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/ingest/rawstores/{}(\\?mode=\\w+){{0,1}}", prefix, stream_name_regex),
+                "POST",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<IngestRawStoreHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            "/proton/v1/search(\\?[\\w\\-=&#]+){0,1}",
-            "POST",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<DB::SearchHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/search(\\?[\\w\\-=&#]+){{0,1}}", prefix),
+                "POST",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<DB::SearchHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            "/proton/v1/pipeline_metrics",
-            "POST",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<DB::PipelineMetricHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/pipeline_metrics", prefix),
+                "POST",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<DB::PipelineMetricHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            "/proton/v1/ingest/statuses",
-            "POST",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<IngestStatusHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/ingest/statuses", prefix),
+                "POST",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<IngestStatusHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            /// PATH: '/proton/v1/ddl/streams [/{databse}] [/{key}] ...'
-            "/proton/v1/ddl/streams(/?(\\?[\\w\\-=&#]+){0,1}$|/(?P<database>[%\\w]+)(/?(\\?[\\w\\-=&#]+){0,1}$|/(?P<stream>[%\\-\\.\\w]+)(\\?[\\w\\-=&#]+){0,1})(\\?[\\w\\-=&#]+){0,1})",
-            "GET",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<TabularTableRestRouterHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                /// PATH: '/proton/v1/ddl/streams [/{databse}] [/{key}] ...'
+                fmt::format(
+                    "/{}/v1/ddl/streams(/?(\\?[\\w\\-=&#]+){{0,1}}$|/(?P<database>[%\\w]+)(/?(\\?[\\w\\-=&#]+){{0,1}}$|/"
+                    "(?P<stream>[%\\-\\.\\w]+)(\\?[\\w\\-=&#]+){{0,1}})(\\?[\\w\\-=&#]+){{0,1}})",
+                    prefix),
+                "GET",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<TabularTableRestRouterHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            "/proton/v1/ddl/streams(?:\\?\?[^/]*)?",
-            "POST",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<TabularTableRestRouterHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/ddl/streams(?:\\?\?[^/]*)?", prefix),
+                "POST",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<TabularTableRestRouterHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            fmt::format("/proton/v1/ddl/streams/{}(\\?[\\w\\-=&#]+){{0,1}}", stream_name_regex),
-            "PATCH/DELETE",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<TabularTableRestRouterHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/ddl/streams/{}(\\?[\\w\\-=&#]+){{0,1}}", prefix, stream_name_regex),
+                "PATCH/DELETE",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<TabularTableRestRouterHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            "/proton/v1/ddl/externalstreams(\\?[\\w\\-=&#]+){0,1}",
-            "POST",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<ExternalStreamRestRouterHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/ddl/externalstreams(\\?[\\w\\-=&#]+){{0,1}}", prefix),
+                "POST",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<ExternalStreamRestRouterHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            "/proton/v1/ddl/rawstores(\\?[\\w\\-=&#]+){0,1}",
-            "GET/POST",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<RawstoreTableRestRouterHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/ddl/rawstores(\\?[\\w\\-=&#]+){{0,1}}", prefix),
+                "GET/POST",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<RawstoreTableRestRouterHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            fmt::format("/proton/v1/ddl/rawstores/{}(\\?[\\w\\-=&#]+){{0,1}}", stream_name_regex),
-            "DELETE" /* So far, not support PATCH */,
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<RawstoreTableRestRouterHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/ddl/rawstores/{}(\\?[\\w\\-=&#]+){{0,1}}", prefix, stream_name_regex),
+                "DELETE" /* So far, not support PATCH */,
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<RawstoreTableRestRouterHandler>(query_context);
+                });
 
-        /// So far, we do not support ALTER TABLE ADD/UPDATE/DELETE COLUMN ...
-        // factory.registerRouterHandler(
-        //     fmt::format("/proton/v1/ddl/{}/columns(\\?[\\w\\-=&#]+){{0,1}}", stream_name_regex),
-        //     "GET/POST",
-        //     [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-        //         return std::make_shared<ColumnRestRouterHandler>(query_context);
-        //     });
+            /// So far, we do not support ALTER TABLE ADD/UPDATE/DELETE COLUMN ...
+            // factory.registerRouterHandler(
+            //     fmt::format("/proton/v1/ddl/{}/columns(\\?[\\w\\-=&#]+){{0,1}}", stream_name_regex),
+            //     "GET/POST",
+            //     [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+            //         return std::make_shared<ColumnRestRouterHandler>(query_context);
+            //     });
 
-        // factory.registerRouterHandler(
-        //     fmt::format("/proton/v1/ddl/{}/columns/(?P<column>[%\\w]+)(\\?[\\w\\-=&#]+){{0,1}}", stream_name_regex},
-        //     "PATCH/DELETE",
-        //     [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-        //         return std::make_shared<ColumnRestRouterHandler>(query_context);
-        //     });
+            // factory.registerRouterHandler(
+            //     fmt::format("/proton/v1/ddl/{}/columns/(?P<column>[%\\w]+)(\\?[\\w\\-=&#]+){{0,1}}", stream_name_regex},
+            //     "PATCH/DELETE",
+            //     [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+            //         return std::make_shared<ColumnRestRouterHandler>(query_context);
+            //     });
 
-        factory.registerRouterHandler(
-            "/proton/v1/ddl/databases(\\?[\\w\\-=&#]+){0,1}",
-            "GET/POST",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<DB::DatabaseRestRouterHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/ddl/databases(\\?[\\w\\-=&#]+){{0,1}}", prefix),
+                "GET/POST",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<DB::DatabaseRestRouterHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            "/proton/v1/ddl/databases/(?P<database>\\w+)(\\?[\\w\\-=&#]+){0,1}",
-            "DELETE",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<DB::DatabaseRestRouterHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/ddl/databases/(?P<database>\\w+)(\\?[\\w\\-=&#]+){{0,1}}", prefix),
+                "DELETE",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<DB::DatabaseRestRouterHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            "/proton/v1/sqlanalyzer",
-            "POST",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<SQLAnalyzerRestRouterHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/sqlanalyzer", prefix),
+                "POST",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<SQLAnalyzerRestRouterHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            "/proton/v1/sqlformat",
-            "POST",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<SQLFormatHandler>(query_context);
-            });
-#if USE_NURAFT
-        /// GET/POST/DELETE: /proton/v1/udfs[/{func}]
-        factory.registerRouterHandler(
-            "/proton/v1/udfs(/?$|/(?P<func>[%\\w]+))",
-            "GET/POST/DELETE",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<UDFHandler>(query_context);
-            });
-#endif
-        factory.registerRouterHandler(
-            "/proton/(?P<status>ping|info)$",
-            "GET",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<DB::PingHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/sqlformat", prefix),
+                "POST",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<SQLFormatHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            "/proton/v1/clusterinfo",
-            "GET",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<DB::ClusterInfoHandler>(query_context);
-            });
+            /// GET/POST/DELETE: /proton/v1/udfs[/{func}]
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/udfs(/?$|/(?P<func>[%\\w]+))", prefix),
+                "GET/POST/DELETE",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<UDFHandler>(query_context);
+                });
 
-        /// GET: /proton/v1/storageinfo[/{database}[/{stream}]]
-        factory.registerRouterHandler(
-            "/proton/v1/storageinfo(/?(\\?[\\w\\-=&#]+){0,1}$|/(?P<database>[%\\w]+)(/?(\\?[\\w\\-=&#]+){0,1}$|/(?P<stream>[%\\-\\.\\w]+)(\\?[\\w\\-=&#]+){0,1})(\\?[\\w\\-=&#]+){0,1})",
-            "GET",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<DB::StorageInfoHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/(?P<status>ping|info)$", prefix),
+                "GET",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<DB::PingHandler>(query_context);
+                });
 
-        /// POST /proton/v1/system?<params>
-        factory.registerRouterHandler(
-            "/proton/v1/system(\\?[\\w\\-=&#]+){0,1}",
-            "POST",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<DB::SystemCommandHandler>(query_context);
-            });
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/clusterinfo", prefix),
+                "GET",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<DB::ClusterInfoHandler>(query_context);
+                });
 
-        factory.registerRouterHandler(
-            "/proton/apis",
-            "GET",
-            [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
-                return std::make_shared<DB::APISpecHandler>(query_context);
-            });
+            /// GET: /proton/v1/storageinfo[/{database}[/{stream}]]
+            factory.registerRouterHandler(
+                fmt::format(
+                    "/{}/v1/storageinfo(/?(\\?[\\w\\-=&#]+){{0,1}}$|/(?P<database>[%\\w]+)(/?(\\?[\\w\\-=&#]+){{0,1}}$|/"
+                    "(?P<stream>[%\\-\\.\\w]+)(\\?[\\w\\-=&#]+){{0,1}})(\\?[\\w\\-=&#]+){{0,1}})",
+                    prefix),
+                "GET",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<DB::StorageInfoHandler>(query_context);
+                });
+
+            /// POST /proton/v1/system?<params>
+            factory.registerRouterHandler(
+                fmt::format("/{}/v1/system(\\?[\\w\\-=&#]+){{0,1}}", prefix),
+                "POST",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<DB::SystemCommandHandler>(query_context);
+                });
+
+            factory.registerRouterHandler(
+                fmt::format("/{}/apis", prefix),
+                "GET",
+                [](ContextMutablePtr query_context) { /// STYLE_CHECK_ALLOW_BRACE_SAME_LINE_LAMBDA
+                    return std::make_shared<DB::APISpecHandler>(query_context);
+                });
+        }
     }
 
     static void registerMetaStoreRestRouterHandlers()
@@ -283,8 +292,10 @@ private:
         {
             throw Exception(
                 ErrorCodes::CANNOT_COMPILE_REGEXP,
-                "Cannot compile re2: '{}' for http handling rule, error: {}. Look at https://github.com/google/re2/wiki/Syntax for reference.",
-                expression, compiled_regex->error());
+                "Cannot compile re2: '{}' for http handling rule, error: {}. Look at https://github.com/google/re2/wiki/Syntax for "
+                "reference.",
+                expression,
+                compiled_regex->error());
         }
 
         return compiled_regex;
