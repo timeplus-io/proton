55169437ba2ebfce259f371237f38f2102e13aee
lizhou1111 <jianan.li@timeplus.com>
2024-06-05 19:07:21 +0800

    porting Clickhouse #45065, Fix Cannot capture column for nested lambda with LowCardinality

--- tests/queries_ported/0_stateless/02990_arrayFold_nullable_lc.sql
+++ tests/queries_ported/0_stateless/02990_arrayFold_nullable_lc.sql
@@ -17,12 +17,12 @@ SELECT array_fold((acc, x) -> acc + x, materialize(cast([1, 2, NULL], 'array(nul
 
 SELECT array_fold((acc, x) -> to_nullable(acc + (x * 2)), [1, 2, 3, 4], to_nullable(to_int64(3)));
 
--- SELECT array_fold((acc, x) -> to_low_cardinality(acc + (x * 2)), [1, 2, 3, 4], to_low_cardinality(to_int64(3)));
+SELECT array_fold((acc, x) -> to_low_cardinality(acc + (x * 2)), [1, 2, 3, 4], to_low_cardinality(to_int64(3)));
 
--- SELECT array_fold((acc, x) -> to_low_cardinality(acc + (x * 2)), [1, 2, 3, 4]::array(low_cardinality(int64)), to_int64(to_low_cardinality(3)));
+SELECT array_fold((acc, x) -> to_low_cardinality(acc + (x * 2)), [1, 2, 3, 4]::array(low_cardinality(int64)), to_int64(to_low_cardinality(3)));
 
 
--- SELECT array_fold((acc, x) -> (acc + (x * 2)), [1, 2, 3, 4], NULL);
--- -- It's debatable which one of the following 2 queries should work, but considering the return type must match the
--- -- accumulator type it makes sense to be the second one
--- SELECT array_fold((acc, x) -> (acc + (x * 2))::low_cardinality(nullable(int64)), [1, 2, 3, 4], NULL::low_cardinality(nullable(int64)));
+SELECT array_fold((acc, x) -> (acc + (x * 2)), [1, 2, 3, 4], NULL);
+-- It's debatable which one of the following 2 queries should work, but considering the return type must match the
+-- accumulator type it makes sense to be the second one
+SELECT array_fold((acc, x) -> (acc + (x * 2))::low_cardinality(nullable(int64)), [1, 2, 3, 4], NULL::low_cardinality(nullable(int64)));
