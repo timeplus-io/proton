602533ea5ef035d3dbe26bb7524dd8e7583dedfa
Saïd <20957603+ayewo@users.noreply.github.com>
2024-07-23 18:06:45 +0100

    blog article #03: Visualizing Nginx Access Logs using Timeplus and Grafana (#798)
    * Intro for article 03.
    
    * add Introduction
    
    * formatting
    
    * WIP
    
    * Docker and related files
    
    * Flesh out section
    
    * code screenshot with line numbers and update others
    
    * add explanation for DDL generation
    
    * finish table of random stream defaults
    
    * explain the structure of nginx_access_log
    
    * reword explanation
    
    * formatting & reword
    
    * done with nginx
    
    * rewordingg
    
    * access log data generation is working
    
    * minor rewording
    
    * focus exclusively on Grafana
    
    * focus on grafana in blog 03
    
    * small refactoring
    
    * 1st line example
    
    * wip
    
    * wip
    
    * update regex to match the image
    
    * reword
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * replace
    
    * wip 2
    
    * wip 3
    
    * wip 3
    
    * wip 4
    
    * replace image
    
    * replace image again
    
    * replace img
    
    * remove
    
    * Add files via upload
    
    * add updated files
    
    * step-by-step
    
    * minor
    
    * add screenshots
    
    * rename file names
    
    * add links to screenshots
    
    * update container name
    
    * automatic provision of dashboard
    
    * replace image
    
    * text edits
    
    * add summary
    
    * add grafana panel images
    
    * add SQL code behind all 5 Grafana panels
    
    * fix images
    
    * Update title

---
+++ examples/nginx-grafana/03_csv-import-01.sql
@@ -0,0 +1,2 @@
+INSERT INTO nginx_historical_access_log (remote_ip, rfc1413_ident, remote_user, date_time_string, http_method, path, http_version, status, size, referer, user_agent, malicious_request) 
+FORMAT CSV
---
+++ examples/nginx-grafana/entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# pause otherwise it fails with "Code: 210. DB::NetException: Connection refused (proton:8463). (NETWORK_ERROR)"
+sleep 10 && proton client --host $PROTON_HOST --multiquery < /tmp/01_nginx-access-log.sql
+
+DEST=/tmp
+export_csv=nginx_export.csv
+import_csv=nginx_import.csv
+export_ipinfo=nginx_export.ipinfo
+import_ipinfo=nginx_import.ipinfo.csv
+
+# export the random stream to a CSV
+proton client --host $PROTON_HOST --multiquery < /tmp/02_csv-export.sql > $DEST/$export_csv
+
+echo "Using the following config for IPinfo:"
+cat ~/.config/ipinfo/config.json
+
+# extract IP addresses from the exported .csv file
+echo "Extracting only IP addresses from the 1st column in $DEST/$export_csv to $DEST/$export_ipinfo.tmp ..."
+cat "$DEST"/$export_csv | cut -d ',' -f 1 | sed 's/^"//; s/"$//' > "$DEST"/"$export_ipinfo.tmp"
+echo "Removing duplicate IP addresses from $DEST/$export_ipinfo.tmp ..."
+sort "$DEST"/"$export_ipinfo.tmp" | uniq > "$DEST"/"$export_ipinfo"
+echo "Combined all IP addresses into a single file: $DEST/$export_ipinfo."
+rm -v "$DEST"/"$export_ipinfo.tmp"
+
+echo "Extracting the rest of the data from $DEST/$export_csv to $DEST/$import_csv ..."
+# cat "$DEST"/$export_csv | cut -d ',' -f 1- > "$DEST"/"$import_csv"
+mv -v "$DEST"/"$export_csv" "$DEST"/"$import_csv"
+
+
+# lookup all the IP addresses using ipinfo.io
+echo "Geo-locating all the IP addresses in bulk using the IPInfo API (https://ipinfo.io):"
+ip_count=$(wc "$DEST"/$export_ipinfo | awk '{print $1}')
+echo "   Total IP addresses that will be looked up in bulk using $DEST/$export_ipinfo: $ip_count." 
+cat "$DEST"/$export_ipinfo | ipinfo -c > "$DEST"/"$import_ipinfo"
+echo "   Geo-lookup of $ip_count IP addresses written to file: $DEST/$import_ipinfo."
+echo "   Complete!"
+
+
+#proton client --host $PROTON_HOST --multiquery < /tmp/03_csv-import.sql
+
+query01=`cat /tmp/03_csv-import-01.sql`
+proton client --host $PROTON_HOST -q "$query01" < /tmp/nginx_import.csv
+
+query02=`cat /tmp/03_csv-import-02.sql`
+proton client --host $PROTON_HOST --multiquery -q "$query02" < /tmp/nginx_import.ipinfo.csv
