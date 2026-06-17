7d465d98c97f7d82ee537d035e460dee7b1158ca
yun <haohang.shi@timeplus.io>
2026-06-17 08:44:54 -0700

    Bump Poco libexpat 2.4.7 → 2.8.1 (track ClickHouse history)
    Co-authored-by: Robert Schulze <robert@clickhouse.com>
    Co-authored-by: Konstantin Bogdanov <konstantin@clickhouse.com>
    Co-authored-by: Antonio Andelic <antonio@clickhouse.com>
    Co-authored-by: Alexey Milovidov <milovidov@clickhouse.com>

--- base/poco/XML/src/xmltok_impl.h
+++ base/poco/XML/src/xmltok_impl.h
@@ -7,19 +7,19 @@
                                  |_| XML parser
 
    Copyright (c) 1997-2000 Thai Open Source Software Center Ltd
-   Copyright (c) 2000 Clark Cooper <coopercc@users.sourceforge.net>
+   Copyright (c) 2000      Clark Cooper <coopercc@users.sourceforge.net>
    Copyright (c) 2017-2019 Sebastian Pipping <sebastian@pipping.org>
    Licensed under the MIT license:
 
    Permission is hereby granted, free of charge, to any person obtaining
-   a copy of this software and associated documentation files (the
+   a copy of this software  and associated documentation files (the
    "Software"), to deal in the Software without restriction, including
    without limitation the rights to use, copy, modify, merge, publish,
    distribute, sublicense, and/or sell copies of the Software, and to permit
    persons to whom the Software is furnished to do so, subject to the
    following conditions:
 
-   The above copyright notice and this permission notice shall be included
+   The above copyright  notice and this permission notice  shall be included
    in all copies or substantial portions of the Software.
 
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
@@ -45,7 +45,7 @@ enum {
   BT_LF,       /* line feed = "\n" */
   BT_GT,       /* greater than = ">" */
   BT_QUOT,     /* quotation character = "\"" */
-  BT_APOS,     /* aposthrophe = "'" */
+  BT_APOS,     /* apostrophe = "'" */
   BT_EQUALS,   /* equal sign = "=" */
   BT_QUEST,    /* question mark = "?" */
   BT_EXCL,     /* exclamation mark = "!" */
